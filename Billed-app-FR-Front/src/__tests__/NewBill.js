bf05be6f1ed523f4d776b6f22ce3590fbeef4dad
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-05-09 10:12:31 +0200

    view fully covered

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -9,11 +9,11 @@ import NewBill from "../containers/NewBill.js"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("Then the envelop icon should be highlighted", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
       //to-do write assertion
-
+      
       
     })
   })
