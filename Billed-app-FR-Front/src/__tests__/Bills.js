ec312a9830b75de50fc44b8e818f56a8f7641758
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-05-10 14:40:41 +0200

    update

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -29,7 +29,6 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills);
       await waitFor(() => screen.getByTestId("icon-window"));
       const windowIcon = screen.getByTestId("icon-window");
-      //to-do write expect expression
       expect(windowIcon.classList.contains("active-icon")).toBe(true);
     });
     test("Then bills should be ordered from earliest to latest", () => {
@@ -44,4 +43,6 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted);
     });
   });
+
+  //describe("");
 });
--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,19 +2,40 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
-
+import { screen } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
+import { ROUTES_PATH } from "../constants/routes";
+import { localStorageMock } from "../__mocks__/localStorage";
+import Router from "../app/Router";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
     test("Then the envelop icon should be highlighted", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      const pathname = ROUTES_PATH["NewBill"];
       //to-do write assertion
-      
-      
-    })
-  })
-})
+
+      //Build DOM
+      Object.defineProperty(window, "location", {
+        value: {
+          path: pathname,
+        },
+      });
+
+      Router();
+
+      expect(
+        screen
+          .getByTestId("icon-mail")
+          .classList.contains("active-icon")
+          .toBeTruthy()
+      );
+    });
+
+    test("When i change the file, it should display the new name of the file", () => {});
+
+    test("When i submit the form, it should redirect the user to the bill page", () => {});
+  });
+});
